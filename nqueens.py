03d07439cbc7469585b8465f5a1a42ef2f3d104b
Player531 <trials9999@gmail.com>
2025-01-14 10:12:04 +0530

    W

--- nqueens.py
+++ nqueens.py
@@ -1,8 +1,30 @@
-def solveNQueens(n):
-    col = set()
-    posDiag = set()
-    negDiag = set()
-    res = []
-    board = [['.'] * n for i in range(n)]
+def solve(col, board, ans, leftrow, upperDiagonal, lowerDiagonal, n):
+    if col == n:
+        ans.append(board[:])
+        return
 
-    
+
+    for row in range(n):
+        if leftrow[row] == 0 and lowerDiagonal[row+col] == 0 and upperDiagonal[n-1+col-row] == 0:
+            board[row] = board[row][:col] + 'Q' + board[row][col+1:]
+            leftrow[row] = 1
+            lowerDiagonal[row+col] = 1
+            upperDiagonal[n-1+col-row] = 1
+            solve(col+1, board, ans, leftrow,upperDiagonal, lowerDiagonal, n)
+            board[row] = board[row][:col] + '.' + board[row][col+1:]
+            leftrow[row] = 0
+            lowerDiagonal[row+col] = 0
+            upperDiagonal[n-1+col-row] = 0
+
+
+def solveNQueens(n):
+    ans = []
+    board = ['.' *n for _ in range(n)]
+    # print(board)
+    leftrow = [0]*n
+    upperDiagonal = [0]*(2*n-1)
+    lowerDiagonal = [0]*(2*n-1)
+    solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
+    print(ans)
+
+solveNQueens(4)
